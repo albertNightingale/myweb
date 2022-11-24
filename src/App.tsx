f04c2d8530847c210bc72c054351245ecb834683
Albert Liu <albertliu@Alberts-MacBook-Pro.local>
2022-11-24 14:59:09 -0700

    fix page not switching errors by converting react-roeuter-dom to version 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Blog from "./pages/Blog";
 import Project from "./pages/Project";
@@ -12,23 +12,13 @@ function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path="/">
-          <Project />
-        </Route>
-        <Route path="/blog">
-          <Blog />
-        </Route>
-        <Route path="/project">
-          <Project />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/resume">
-          <Resume />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Project />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
     </div>
   );
 }
