cd640fd4ebd091f5a537bf407170f27b76da8d08
Albert (jiaming) Liu <70285611+albertNightingale@users.noreply.github.com>
2023-06-23 12:11:50 -0700

    remove AboutMe margin and replace it with padding

--- src/pages/About/AboutMeSection/AboutMe.tsx
+++ src/pages/About/AboutMeSection/AboutMe.tsx
@@ -4,7 +4,7 @@ import AboutMeProfile from './AboutMeImage';
 
 export default function AboutMe() {
   return (
-    <div className="flex flex-row bg-[#b5daff] mt-1 mb-1">
+    <div className="flex flex-row bg-[#b5daff] pt-1 pb-1">
       <AboutMeProfile />
       <div className="basis-4/5	flex flex-col font-mono p-2 text-center">
         <div className="text-8xl font-bold text-zinc-900 tracking-widest ">Albert Liu</div>
