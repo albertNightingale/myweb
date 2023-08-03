1826c2d14a76056fe3fbd0a35350ecc2812644f9
Albert (jiaming) Liu <70285611+albertNightingale@users.noreply.github.com>
2023-08-02 21:18:32 -0700

    add changes url of gitcontribution and projects api call

--- src/apihooks/useGitContribution.ts
+++ src/apihooks/useGitContribution.ts
@@ -1,11 +1,25 @@
 import { useQuery } from 'react-query';
+import { GitContributionType } from '../types';
 
-export default () => {
+import urlJoin from 'url-join';
+
+async function fetchGithubContribution() {
+  const endpointURL = urlJoin(
+    `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_IP}`,
+    process.env.REACT_APP_ENV as string,
+    "githubContribution"
+  );
+
+  // console.log(endpointURL);
+  return fetch(endpointURL, { method: "GET" })
+    .then((response) => response.json());
+}
+
+export default (): { data: GitContributionType, isLoading: boolean, error: any } => {
   const {
     data, isLoading, error,
-  } = useQuery('fetch github contribution', async () => {
-    return fetch(`${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_IP}/githubContribution`, { method: "GET" })
-      .then((response) => response.json());
+  } = useQuery('fetch github contribution', fetchGithubContribution, {
+    staleTime: Infinity
   });
 
   return {
--- src/apihooks/useProjects.ts
+++ src/apihooks/useProjects.ts
@@ -1,11 +1,24 @@
 import { useQuery } from "react-query";
+import { Project } from "../types";
+import urlJoin from 'url-join';
 
-export default () => {
+async function fetchProjects() {
+  const endpointURL = urlJoin(
+    `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_IP}`,
+    process.env.REACT_APP_ENV as string,
+    "projects"
+  );
+
+  // console.log(endpointURL);
+  return fetch(endpointURL, { method: "GET" })
+    .then((response) => response.json());
+}
+
+export default (): { data: Project[], isLoading: boolean, error: any } => {
   const {
     data, isLoading, error,
-  } = useQuery('fetch projects', async () => {
-    return fetch(`${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_IP}/projects`, { method: "GET" })
-      .then((response) => response.json());
+  } = useQuery('fetch projects', fetchProjects, {
+    staleTime: Infinity
   });
 
   return {
